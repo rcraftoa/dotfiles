57441b53a7a5aee716f66a951f84f0d24dbe9db2
rcraftzy <roberto.tg99@gmail.com>
2024-03-10 22:22:19 -0500

    Add ui config for tranparency, drop bufferline and change theme to status line

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -2,6 +2,17 @@ local M = {}
 
 M.ui = {
   theme = "solarized_osaka",
+
+  tabufline = {
+    enabled = false,
+  },
+
+  transparency = false,
+
+  statusline = {
+    theme = "vscode_colored"
+  }
+
 }
 
 return M

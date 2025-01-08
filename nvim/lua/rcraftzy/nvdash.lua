2548c8cbce32166abed8035892e71121a5ab7483
rcraftzy <roberto.tg99@gmail.com>
2025-01-07 21:18:30 -0500

    feat(nvim): add a button nvdash to open configs nvchad

--- nvim/lua/rcraftzy/nvdash.lua
+++ nvim/lua/rcraftzy/nvdash.lua
@@ -12,6 +12,7 @@ return {
   buttons = {
     { txt = "  Find File", keys = "Spc f f", cmd = "Telescope find_files" },
     { txt = "  Recent Files", keys = "Spc f o", cmd = "Telescope oldfiles" },
+    { txt = "  Config", keys = "Spc f P", cmd = ":lua require('telescope.builtin').find_files ({ cwd = '~/dotfiles/nvim/lua/', })"},
     -- more... check nvconfig.lua file for full list of buttons
   },
 }

b8004ff613079a3fc078ff7735a9c0031bbb3c5e
rcraftzy <roberto.tg99@gmail.com>
2025-06-22 00:13:26 -0500

    feat: update config flake nix darwin

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -21,7 +21,7 @@
           pkgs.starship
           pkgs.neovim
           pkgs.nushell
-          pkgs.alacritty
+          # pkgs.alacritty
           pkgs.mkalias
           pkgs.tmux
           pkgs.fnm
@@ -33,29 +33,8 @@
         ];
       fonts.packages = [
       ];
-
-      system.activationScripts.applications.text = let
-        env = pkgs.buildEnv {
-        name = "system-applications";
-        paths = config.environment.systemPackages;
-        pathsToLink = "/Applications";
-      };
-      in
-        pkgs.lib.mkForce ''
-          # Set up applications.
-          echo "setting up /Applications..." >&2
-          rm -rf /Applications/Nix\ Apps
-          mkdir -p /Applications/Nix\ Apps
-          find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-          while read src; do
-          app_name=$(basename "$src")
-          echo "copying $src" >&2
-          ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-          done
-        '';
-
       # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
+      # services.nix-daemon.enable = true;
       # nix.package = pkgs.nix;
 
       # Necessary for using flakes on this system.
@@ -76,8 +55,8 @@
 
       users.users.rcraftzy.home = "/Users/rcraftzy";
       home-manager.backupFileExtension = "backup";
-      nix.configureBuildUsers = true;
-      nix.useDaemon = true;
+      # nix.configureBuildUsers = true;
+      # nix.useDaemon = true;
     };
   in
   {
